b2227cd6998b7d8855a498853cbda8960d177136
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-01-04 13:46:46 +0800

    Refactor database module and ORM class

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,79 +1,77 @@
-import mysql, { type Pool, type RowDataPacket } from "mysql2/promise";
+import mysql from "mysql2/promise";
+import type { RowDataPacket, ResultSetHeader } from "mysql2/promise";
 
-let pool: Pool | null = null;
+let pool: mysql.Pool | null;
 
 export class Database {
   static async get() {
-    if (pool) return pool;
-    // @ts-ignore
-    pool = await mysql.createPool({
+	if (pool) return pool.getConnection();
+    pool = mysql.createPool({
       host: "localhost",
       user: "root",
       password: "",
       database: "cs127",
     });
-    return pool!;
+    return pool!.getConnection();
   }
+}
+
+const select = async (table: string, options: any = {}) => {
+	const attributes = options.attributes || ['*'];
+	const where = options.where
+		? `WHERE ${Object.keys(options.where)
+				.map((attribute: any) => `${attribute} = '${options.where[attribute]}'`)
+				.join(' AND ')}`
+		: '';
+	const query = `SELECT ${attributes.join(', ')} FROM ${table} ${where};`;
+	return await pool!.execute<RowDataPacket[]>(query);
+}
+
+const insert = async (table: string, data: any) => {
+	const attributes = Object.keys(data);
+	const values = attributes.map((attribute: any) => `'${data[attribute]}'`);
+	const query = `INSERT INTO ${table} (${attributes.join(', ')}) VALUES (${values.join(', ')});`;
+	return await pool!.execute<ResultSetHeader>(query);
+}
+
+const update = async (table: string, data: any, options: any = {}) => {
+	const attributes = Object.keys(data);
+	const values = attributes.map((attribute: any) => `${attribute} = '${data[attribute]}'`);
+	const where = options.where
+		? `WHERE ${Object.keys(options.where)
+				.map((attribute: any) => `${attribute} = '${options.where[attribute]}'`)
+				.join(' AND ')}`
+		: '';
+	const query = `UPDATE ${table} SET ${values.join(', ')} ${where};`;
+	return await pool!.execute<ResultSetHeader>(query);
+}
+
+const remove = async (table: string, options: any = {}) => {
+	const where = options.where
+		? `WHERE ${Object.keys(options.where)
+				.map((attribute: any) => `${attribute} = '${options.where[attribute]}'`)
+				.join(' AND ')}`
+		: '';
+	const query = `DELETE FROM ${table} ${where};`;
+	return await pool!.execute<ResultSetHeader>(query);
 }
 
 class ORM {
 	[key: string]: any;
-	constructor(pool: mysql.Pool) {
-		pool.execute<mysql.RowDataPacket[]>('SHOW TABLES').then(async ([rows]) => {
+	constructor() {
+		if (!pool) Database.get();
+		pool!.execute<RowDataPacket[]>('SHOW TABLES').then(async ([rows]) => {
 			Object.values(rows).map((row: RowDataPacket) => {
 				const table = row[`Tables_in_cs127`];
 				this[table] = {
-					select: async (options: any = {}) => {
-						const attributes = options.attributes || ['*'];
-						const where = options.where
-							? `WHERE ${Object.keys(options.where)
-									.map((attribute: any) => `${attribute} = '${options.where[attribute]}'`)
-									.join(' AND ')}`
-							: '';
-						const query = `SELECT ${attributes.join(', ')} FROM ${table} ${where};`;
-						return await pool.execute<mysql.RowDataPacket[]>(query);
-					},
-					insert: async (data: any) => {
-						const attributes = Object.keys(data);
-						const values = attributes.map((attribute: any) => `'${data[attribute]}'`);
-						const query = `INSERT INTO ${table} (${attributes.join(', ')}) VALUES (${values.join(
-							', '
-						)});`;
-						return await pool.execute<mysql.ResultSetHeader>(query);
-					},
-					update: async (data: any, options: any = {}) => {
-						const attributes = Object.keys(data);
-						const values = attributes.map(
-							(attribute: any) => `${attribute} = '${data[attribute]}'`
-						);
-						const where = options.where
-							? `WHERE ${Object.keys(options.where)
-									.map((attribute: any) => `${attribute} = '${options.where[attribute]}'`)
-									.join(' AND ')}`
-							: '';
-						const query = `UPDATE ${table} SET ${values.join(', ')} ${where};`;
-						return await pool.execute<mysql.ResultSetHeader>(query);
-					},
-					delete: async (options: any = {}) => {
-						const where = options.where
-							? `WHERE ${Object.keys(options.where)
-									.map((attribute: any) => `${attribute} = '${options.where[attribute]}'`)
-									.join(' AND ')}`
-							: '';
-						const query = `DELETE FROM ${table} ${where};`;
-						return await pool.execute<mysql.ResultSetHeader>(query);
-					}
+					select: select.bind(null, table),
+					insert: insert.bind(null, table),
+					update: update.bind(null, table),
+					remove: remove.bind(null, table),
 				};
 			});
 		});
 	}
 }
 
-export default new ORM(
-	await mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "cs127",
-	})
-);
+export default new ORM();
